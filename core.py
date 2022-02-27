910b84f0454052998526a86ff673072b2cd57c4f
Tom Mong <mongyin98@gmail.com>
2022-02-27 14:14:38 +0800

    Remove default access

--- core.py
+++ core.py
@@ -5,13 +5,17 @@ import json
 import pathlib
 import os
 
+from decorator import Error_Handler
+
+
 class HKTVmallToolkit:
     """HKTVmallToolKit.
 
     This module used to interact with the HKTVmall Exchange backend, which used mainly for inventory and order management
 
     """
-    def __init__(self,location="sales-record/",username=os.environ['HKTV_USERNAME'],pwd=os.environ['HKTV_PWD'],merchant=os.environ['HKTV_MERCHANT_CODE']):
+    @Error_Handler
+    def __init__(self, username, pwd, merchant, location="sales-record/", maxRetry=5):
         """__init__
 
         The method initialize the object with its location, username, password and merchant id
@@ -25,50 +29,72 @@ class HKTVmallToolkit:
             pwd      (str): The login password used to login https://exchange.hktvmall.com
             merchant (str): The merchant id of your HKTVmall store that start with H (i.e. HXXXXXXX)
         """
-        self.location = location
         self.username = username
         self.pwd = pwd
         self.merchant = merchant
+        self.location = location
+        self.maxRetry = maxRetry
 
-        # Create the location 
+        # login to HKTVmall
+        for count in range(self.maxRetry):
+            try:
+                self.cookies = self.__loginHKTVmall()
+                break
+            except:
+                raise Exception(
+                    f"[Try: {count+1}] Can't not login to HKTVmall")
+
+        if self.cookies == "":
+            raise Exception("No cookies found")
+
+        # Create the location
         path = pathlib.Path(location)
         if not path.exists():
             # Create the location
             os.mkdir(location)
 
-    def __getHKTVmallDailySales(self,date,cookies,location,chuck_size=512):
+    @Error_Handler
+    def __getHKTVmallDailySales(self, date, location, chuck_size=512):
         """__getHKTVmallDailySales
 
         The method will get the daily sales excel from the HKTVmall
 
         Args:
             date        (str) : The date of the report you want to get in the format of {yyyymmdd} (i.e. 20220212)
-            cookies     (dict): The cookies contains PHPSESSID
             location    (str) : The saving location (path) of the sales report
             chuck_size  (int) : The chuck size of the file
 
         Return:
             filePath (str): The location of the report
         """
-        try:
-            url = "https://exchange.hktvmall.com/merchant/reports/download_report.php"
-            merchant = self.merchant
-            fileName = f"ECOM-EXCH_DAILY_ORDER_{merchant}_{date}.xlsx"
-            r = requests.post(url, data={"fileName": fileName, "reportDataPath": "../merchant_reports/"}, cookies=cookies, stream=True)
-            
-            if r.text.startswith("File Not Exist"):
-                raise Exception(f"The file {fileName} not exist")
 
-            filePath = location + fileName
-            with open(filePath, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=chuck_size): 
-                    f.write(chunk)
-                return filePath
-        except Exception as msg:
-            print(msg)
-            return False
+        fileName = f"ECOM-EXCH_DAILY_ORDER_{self.merchant}_{date}.xlsx"
+        url = "https://exchange.hktvmall.com/merchant/reports/download_report.php"
 
-    def __getDay(self,last=1):
+        for _ in range(self.maxRetry):
+            r = requests.post(url, data={"fileName": fileName, "reportDataPath": "../merchant_reports/"}, cookies={"PHPSESSID": self.cookies}, stream=True)
+
+            if len(r.history) != 0:
+                # The page got redirected: login crediential not working
+                print("Cookies not working! Getting a new one")
+                self.cookies = self.__loginHKTVmall()
+
+            else:
+                # Succesfully Login, then break the for loop
+                break
+
+        if r.text.startswith("File Not Exist"):
+            raise Exception(f"The file {fileName} not exist")
+
+        filePath = location + fileName
+
+        with open(filePath, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=chuck_size):
+                f.write(chunk)
+            return filePath
+
+    @Error_Handler
+    def __getDay(self, last=1):
         """__getDay
 
         The method will return the date of n day before today.
@@ -82,33 +108,37 @@ class HKTVmallToolkit:
         today = datetime.date.today()
         yesterday = today - datetime.timedelta(days=last)
 
-        return yesterday.strftime('%Y%m%d')
+        return yesterday.strftime("%Y%m%d")
 
+    @Error_Handler
     def __loginHKTVmall(self):
         """__loginHKTVmall
 
         The method will login to https://exchange.hktvmall.com and return the session cookies
 
         Return:
-            cookies (dict): The PHPSESSID cookies (i.e. {"PHPSESSID": "xxxxxxxxxxx"})
+            cookies (str): The PHPSESSID
         """
         url = "https://exchange.hktvmall.com/merchant/shared/login.php"
         username = self.username
         pwd = self.pwd
-        
-        r = requests.post(url, data={'username': username, 'pwd':pwd})
-        
+
+        r = requests.post(url, data={'username': username, 'pwd': pwd})
+
         # Get PHPSESSID
         for c in r.cookies:
             if c.name == "PHPSESSID":
-                return {c.name:c.value}
-        return False
+                return c.value
 
-    def getSalesND(self,date=0):
+        return Exception("Unable to Login")
+
+    @Error_Handler
+    def getSalesND(self, date=0):
         target_date = self.__getDay(date)
         return self.getSales(target_date)
 
-    def getSales(self,date=""):
+    @Error_Handler
+    def getSales(self, date=""):
         """getSales
 
         The method will return the daily sales of a given day
@@ -121,28 +151,29 @@ class HKTVmallToolkit:
         """
         if date == "":
             date = self.__getDay()
-        cookies = self.__loginHKTVmall()
-        if cookies == False:
-            raise Exception("Unable to Login")
-        fileName = self.__getHKTVmallDailySales(date=date,cookies=cookies,location=self.location)
 
-        print(fileName)
+        fileName = self.__getHKTVmallDailySales(
+            date=date, location=self.location)
 
         try:
-            df = pd.read_excel(fileName,skiprows=4)
+            df = pd.read_excel(fileName, skiprows=4)
         except Exception as msg:
             try:
                 os.remove(fileName)
             except:
                 pass
-            raise Exception("Cannot read excel",msg)
+            raise Exception("Cannot read excel", msg)
         result = df.to_json(orient="table")
         parsed = json.loads(result)
 
         return parsed["data"]
 
-if __name__ == '__main__':
-    tbox = HKTVmallToolkit()
 
-    sales_data = tbox.getSales()
-    print(sales_data)
+if __name__ == '__main__':
+    try:
+        tbox = HKTVmallToolkit(username="H7225001_SM",
+                               pwd="nW$$$47777", merchant="H7225001")
+        sales_data = tbox.getSales()
+        print(sales_data)
+    except Exception as msg:
+        print(msg)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from schema import Query
 from core import HKTVmallToolkit as Toolkit
@@ -8,7 +8,7 @@ from core import HKTVmallToolkit as Toolkit
 app = FastAPI(
     title="HKTVmall Exchange Sales API",
     description="This API used to interact with the HKTVmall Exchange backend",
-    version="1.0.1",
+    version="1.2.0",
     contact={
         "name": "Tom Mong from Stocksgram",
         "url": "https://stocksgram.com",
@@ -17,49 +17,57 @@ app = FastAPI(
     docs_url='/'
 )
 
-@app.get("/sales/nday/{nday}")
-def get_Sales_with_nday(nday: int):
-    """
-    Get the sales of nday before today
-    """
-    tkit = Toolkit()
-
-    results = tkit.getSalesND(nday)
-    return results
-
 @app.post("/sales/nday/{nday}")
 async def get_Sales_with_nday_with_login(nday: int, q: Query):
     """
     Get the sales of nday before today
     """
-    if q == None:
-        tkit = Toolkit()
-    else:
+    try:
         tkit = Toolkit(username=q.username,pwd=q.password,merchant=q.merchant)
 
-    results = tkit.getSalesND(nday)
-    return results
-
-
-@app.get("/sales/date/{date}")
-def get_Sales_with_date(date: str):
-    """
-    Get the sales of a particular day
-    """
-    tkit = Toolkit()
-
-    results = tkit.getSales(date)
-    return results
+        results = tkit.getSalesND(nday)
+        return results
+    except Exception as msg:
+        raise HTTPException(status_code=500, detail=repr(msg))
 
 @app.post("/sales/date/{date}")
 async def get_Sales_with_date_with_login(date: int, q: Query):
     """
     Get the sales of a particular day
     """
-    if q == None:
-        tkit = Toolkit()
-    else:
+    try:
         tkit = Toolkit(username=q.username,pwd=q.password,merchant=q.merchant)
 
-    results = tkit.getSales(date)
-    return results
+        results = tkit.getSales(date)
+        return results
+    except Exception as msg:
+        raise HTTPException(status_code=500, detail=repr(msg))
+
+# Deprecidated 
+
+# @app.get("/sales/nday/{nday}")
+# def get_Sales_with_nday(nday: int):
+#     """
+#     Get the sales of nday before today
+#     """
+#     try:
+#         tkit = Toolkit()
+
+#         results = tkit.getSalesND(nday)
+#         return results
+#     except Exception as msg:
+#         raise HTTPException(status_code=500, detail=repr(msg))
+
+
+# @app.get("/sales/date/{date}")
+# def get_Sales_with_date(date: str):
+#     """
+#     Get the sales of a particular day
+#     """
+#     try:
+#         tkit = Toolkit()
+
+#         results = tkit.getSales(date)
+#         return results
+#     except Exception as msg:
+#         raise HTTPException(status_code=500, detail=repr(msg))
